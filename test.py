d34f41b67384a979537822f9bb06c4a4d8b9b33f
Andreja Turk <andreja.turk@student.fmf.uni-lj.si>
2019-04-24 15:10:00 +0200

    spremembe, da ne bo več delalo

--- test.py
+++ test.py
@@ -4,8 +4,13 @@ def je_prastevilo(n):
     for i in range(2, n):
         if n % i == 0:
             return False
-    return True
+    return True and neki
 
 for x in range( 2, 201):
     if je_prastevilo(x):
-        print(x)
+        print
+
+
+
+        pass 
+        ansaisjo
