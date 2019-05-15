1c2f71cdbba1854e06cb3cd1810050935b4e94ed
Andreja Turk <andreja.turk@student.fmf.uni-lj.si>
2019-05-15 15:31:06 +0200

    definiram razred igra

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ PONOVLJENA_CRKA = "O"
 NAPACNA_CRKA = "-"
 ZMAGA = "W"
 PORAZ = "X"
+#za metodo ugibaj: te štiri možnosti imamo, ko ugibamo
 
 class Igra:
 
@@ -43,12 +44,33 @@ class Igra:
                 delni += "_ "
         return delni
     
-    
+
+    def nepravilni_ugibi(self):
+        return " ". join(self.napacne_crke())
+
+    def ugibaj(self, crka):
+        crka = crka.upper()
+        if crka in self.crke:
+            return PONOVLJENA_CRKA
+        else:
+            self.crke.append(crka)
+        if crka in self.geslo:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
 
 
 
-testno_geslo = "požrtvovalnost"
-testne_crke = ["a", "e", "o", "p"]
+
+
+testno_geslo = "požrtvovalnost".upper()
+testne_crke = ["A", "E", "O", "P"]
 zmagovalne_crke = [x for x in testno_geslo]
 igra = Igra(testno_geslo, testne_crke)
 print(igra.napacne_crke())
@@ -58,3 +80,14 @@ print(igra.zmaga())
 zmagana_igra = Igra(testno_geslo, zmagovalne_crke)
 print(zmagana_igra.zmaga())
 print(igra.pravilni_del_gesla())
+print(igra.nepravilni_ugibi())
+poskus = igra.ugibaj("r")
+print(poskus)
+print(igra.pravilni_del_gesla())
+poskus = igra.ugibaj("E")
+print(poskus)
+print(igra.pravilni_del_gesla())
+poskus = igra.ugibaj("x")
+print(poskus)
+print(igra.pravilni_del_gesla())
+print(igra.napacne_crke())
